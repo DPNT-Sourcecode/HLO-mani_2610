eaa6dc7e6440b9e595982bc3917c42c2414d2cee
sbx_user1060 <sbx_user1060@ip-10-129-54-201.eu-west-2.compute.internal>
2018-10-26 09:11:38 +0000

    Fri Oct 26 09:11:38 UTC 2018

--- src/main/scala/befaster/solutions/SUM/Sum.scala
+++ src/main/scala/befaster/solutions/SUM/Sum.scala
@@ -3,5 +3,5 @@ package befaster.solutions.SUM
 import befaster.runner.SolutionNotImplementedException
 
 object Sum {
-  def sum(x: Int, y: Int): Int = throw new SolutionNotImplementedException()
+  def sum(x: Int, y: Int): Int = x + y
 }
